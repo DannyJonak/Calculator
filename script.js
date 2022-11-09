0c62944db263ba32d87c29c210a871670fece911
Danny Jonak <dannyjonak@gmail.com>
2022-11-09 12:38:57 -0500

    implement basic operation functions

--- script.js
+++ script.js
@@ -15,4 +15,16 @@ function division(num1, num2) {
     return num2==0? NaN: num1 / num2;
 }
 
-function operate()
+function operate(num1, num2, operator) {
+    switch(operator) {
+       
+        case '+':
+            return addition(num1, num2);
+        case '-':
+            return subtraction(num1, num2);
+        case '*':
+            return multiplication(num1, num2);
+        case '/':
+            return division(num1, num2);
+    }
+}
