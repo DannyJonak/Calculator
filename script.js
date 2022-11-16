e92ab17a90774c3a76b399231c33d89ff56b7b08
Danny Jonak <dannyjonak@gmail.com>
2022-11-16 14:47:30 -0500

    implement classes for input and calculator

--- script.js
+++ script.js
@@ -15,10 +15,6 @@ function division(num1, num2) {
     return num2==0? NaN: num1 / num2;
 }
 
-function plusMinus(num) {
-    return -1 * num;
-}
-
 function operate(operator, ...nums) {
     switch(operator) {
        
@@ -30,7 +26,46 @@ function operate(operator, ...nums) {
             return multiplication(nums[0], nums[1]);
         case '/':
             return division(nums[0], nums[1]);
-        case '+/-':
-            return plusMinus(nums[0]);
     }
 }
+
+const inputStream = {
+    bufferZone: '',
+    maxSize: 9,
+    clearBuffer: function() {
+        this.bufferZone = '';
+    },
+    addChar: function(char) {
+        if(this.bufferZone.length < this.maxSize) {
+            if(char === '.') {
+                if(!this.bufferZone.includes('.')) {
+                    this.bufferZone += char;
+                }
+            } else {
+                this.bufferZone += char;
+            }
+        }
+    },
+    removeChar: function() {
+        if (this.bufferZone.length > 0) {
+            this.bufferZone = this.bufferZone.slice(0, this.bufferZone.length - 1);
+        }
+    },
+    getInput: function() {
+        return parseFloat(this.bufferZone);
+    },
+};
+
+const calculator = {
+    prevAns: null,
+    operation: null,
+    compute: function() {
+        if(this.prevAns != null && inputStream.bufferZone.length > 0 && this.operation != null) {
+            nextArg = inputStream.getInput();
+            this.prevAns = operate(this.operation, this.prevAns, nextArg);
+            return this.prevAns;
+        }
+    },
+};
+
+
